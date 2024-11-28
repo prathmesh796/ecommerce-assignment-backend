40125a0f66c01c172f9b21f35e4c28304f848218
Prathmesh Ravindra Salunkhe <salunkheprathmesh0@gmail.com>
2024-11-28 23:38:22 +0530

    removing .env

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -9,16 +9,13 @@ router.post('/signup', async (req, res) => {
     const { email, password } = req.body;
 
     try {
-        // Check if the user already exists
         const existingUser  = await User.findOne({ email });
         if (existingUser ) {
             return res.status(400).json({ message: 'User  already exists' });
         }
 
-        // Hash the password
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        // Create a new user
         const newUser  = new User({ email, password: hashedPassword });
         await newUser.save();
 
