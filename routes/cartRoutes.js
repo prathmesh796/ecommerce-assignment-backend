8a52032a7aa3c263c911c0c6ed7ae64912ee2cdb
Prathmesh Ravindra Salunkhe <salunkheprathmesh0@gmail.com>
2024-11-29 15:01:44 +0530

    fixed cart synnchronization

--- routes/cartRoutes.js
+++ routes/cartRoutes.js
@@ -24,8 +24,17 @@ router.post('/add', async (req, res) => {
 
 // View cart
 router.get('/:userId', async (req, res) => {
-    const cart = await Cart.findOne({ userId: req.params.userId }).populate('items.productId');
-    res.json(cart);
+    const { userId } = req.params;
+    try {
+        const cart = await Cart.findOne({ userId }).populate('items.productId');
+        if (!cart) {
+            return res.status(404).json({ message: 'Cart not found' });
+        }
+        res.json(cart);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Server error' });
+    }
 });
 
 // Remove item from cart
